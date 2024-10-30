2b9e9f96551754a0c089c8fb079d548d8f8f9fcb
Ashraf2060 <118954369+Ashraf2060@users.noreply.github.com>
2024-10-30 11:27:49 +0300

    third

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,12 +3,16 @@
 import { PrismaClient } from '@prisma/client';
 
 declare global {
-  // Allow global `var` declarations for hot reloading in development
+  // Declare a global var for PrismaClient to reuse across re-renders in development.
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma || new PrismaClient({
-  log: ['query'], // Optional: Log queries in development
-});
+// Initialize the Prisma client, reusing the global instance if available
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn'] : ['error'],
+  });
 
+// Assign Prisma client instance to the global var in development to prevent reinitialization
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
